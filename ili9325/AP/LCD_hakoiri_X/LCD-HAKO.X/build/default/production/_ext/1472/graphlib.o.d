89b8066724c816686cac74d94c24e382f07612ab
dozan5 <dozan777@gmail.com>
2022-03-13 19:34:52 +0900

    ili9325

---
+++ ili9325/AP/LCD_hakoiri_X/LCD-HAKO.X/build/default/production/_ext/1472/graphlib.o.d
@@ -0,0 +1,6 @@
+build/default/production/_ext/1472/graphlib.o: ../graphlib.c \
+ ../graphlib.h ../LCDdriver.h
+
+../graphlib.h:
+
+../LCDdriver.h:
---
+++ ili9325/AP/LCD_invader_X/LCD-INVD.X/build/default/production/_ext/1472/LCD_invader.o.d
@@ -0,0 +1,8 @@
+build/default/production/_ext/1472/LCD_invader.o: ../LCD_invader.c \
+ ../LCDdriver.h ../graphlib.h ../recscore.h
+
+../LCDdriver.h:
+
+../graphlib.h:
+
+../recscore.h:
