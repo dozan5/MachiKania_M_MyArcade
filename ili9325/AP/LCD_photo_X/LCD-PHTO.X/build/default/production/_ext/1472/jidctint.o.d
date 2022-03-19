7e685e51c48e008f45a3f4da682dcb564c28d1b8
dozan5 <dozan777@gmail.com>
2022-03-20 08:13:44 +0900

    temp

--- ili9325/AP/LCD_photo_X/LCD-PHTO.X/build/default/production/_ext/1472/jidctint.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1472/jidctint.o: ../jidctint.c \
- ../GenericTypeDefs.h
-
-../GenericTypeDefs.h:
--- ili9341/AP/LCD_veludda_X/veludda_lcd.X/build/default/production/_ext/1472/veludda_chars.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1472/veludda_chars.o: ../veludda_chars.c \
- ../veludda.h
-
-../veludda.h:
